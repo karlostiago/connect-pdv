62ca099751610931e7110f2b81c8319cad38066c
Carlos Tiago <carlostiagodesousa@gmail.com>
2024-04-07 11:45:24 -0300

    refact: atualizacao do spring e versao do java

--- pdv/src/main/java/com/connectpdv/pdv/ApplicationStarter.java
+++ pdv/src/main/java/com/connectpdv/pdv/ApplicationStarter.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PdvApplication {
+public class ApplicationStarter {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PdvApplication.class, args);
+		SpringApplication.run(ApplicationStarter.class, args);
 	}
 
 }
