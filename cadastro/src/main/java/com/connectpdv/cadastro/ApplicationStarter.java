fcd55628a0463f365f77e3d3dfe670cbce88a17e
Carlos Tiago <carlostiagodesousa@gmail.com>
2024-04-07 11:42:41 -0300

    refact: atualizacao da versao do spring e versao do java

--- cadastro/src/main/java/com/connectpdv/cadastro/ApplicationStarter.java
+++ cadastro/src/main/java/com/connectpdv/cadastro/ApplicationStarter.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CadastroApplication {
+public class ApplicationStarter {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CadastroApplication.class, args);
+		SpringApplication.run(ApplicationStarter.class, args);
 	}
 
 }
