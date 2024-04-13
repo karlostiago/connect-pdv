95a956845d7c4c5dcc2b30f14d4b3595007b0856
Carlos Tiago <carlostiagodesousa@gmail.com>
2024-04-13 18:15:13 -0300

    feat: criacao do modulo fiscal

---
+++ fiscal/src/test/java/com/connectpdv/fiscal/FiscalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connectpdv.fiscal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiscalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
